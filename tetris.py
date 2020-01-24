59da7c3a53ab89d1e14e01039f8259aded40fb39
IzhetskiyVitalik <56772217+IzhetskiyVitalik@users.noreply.github.com>
2020-01-24 21:46:01 +0300

    Add files via upload

--- tetris.py
+++ tetris.py
@@ -3,7 +3,8 @@ import pygame, random
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN)
 pygame.display.set_caption('Tetris')
-game = 0
+tetris_fon = pygame.image.load("tetris_fon.png")
+data = []
 
 
 #функция для написания текста
@@ -11,10 +12,7 @@ def write(text, color, pos, size=75):
     font = pygame.font.Font(None, size)
     pos = list(pos)
     txt = font.render(text, False, color)
-    if pos[0] == None:
-        pos[0] = (400 - txt.get_width()) // 2
     screen.blit(txt, pos)
-
 
 #хранение фигурок в массиве
 figures = (
@@ -32,35 +30,8 @@ figures = (
     (((-1, 0), (0, 0), (0, 1), (1, 1)), ((1, -1), (1, 0), (0, 0), (0, 1)))
 )
 
-#начало игры
-while game == 0:
-    screen.fill((0, 0, 0))
-    x, y = pygame.mouse.get_pos()
-    text = 'PLAY'
-    yep = False
-    rect = (760, 440, 400, 200)
-    txt_pos = (820, 490)
-    bg = (255, 255, 255)
-    txt_color = (0, 0, 0)
-    active_bg = (180, 180, 180)
-    active_txt_color = (80, 80, 80)
-    size = 150
-    pygame.draw.rect(screen, bg, rect)
-    write(text, txt_color, txt_pos, size)
-    if rect[0] < x < rect[0] + rect[2] and rect[1] < y < rect[1] + rect[3]:
-        pygame.draw.rect(screen, active_bg, rect)
-        write(text, active_txt_color, txt_pos, size)
-        yep = True
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        if event.type == pygame.MOUSEBUTTONDOWN and yep:
-            game = 1
-    pygame.display.flip()
-
-
-#сама игра
-def drawer():
+#отрисовка игры
+def drawer(score, rows, next_figure, next_type, next_color, bricks, figure, type, pos, color):
     screen.fill((50, 215, 200))
     screen.blit(tetris_fon, (512, 0))
     write("SCORE:", (0, 0, 0), (1080, 120), 40)
@@ -71,10 +42,6 @@ def drawer():
     pygame.draw.rect(screen, (0, 0, 0), (1120, 360, 81, 81))
     for i in next_figure[next_type]:
         pygame.draw.rect(screen, next_color, (1141 + i[0] * 20, 381 + i[1] * 20, 19, 19))
-    if red_pased:
-        screen.blit(red_button2, (620, 730))
-    else:
-        screen.blit(red_button1, (620, 730))
     pygame.draw.rect(screen, (0, 0, 0), (662, 79, 401, 600))
     # отрисовка уже лежащих
     for i in range(len(bricks)):
@@ -83,149 +50,203 @@ def drawer():
                 pygame.draw.rect(screen, bricks[i][j], (663 + i * 20, 79 + j * 20, 19, 19))
     # сетка
     for i in range(1, 20):
-        pygame.draw.line(screen, (100, 100, 100), (662 + i * 20, 78), (662 + i * 20, 678))
+        pygame.draw.line(screen, (100, 100, 100), (662 + i * 20, 79), (662 + i * 20, 678))
     for i in range(1, 30):
         pygame.draw.line(screen, (100, 100, 100), (662, 78 + i * 20), (1062, 78 + i * 20))
     #####
     for i in figure[type]:
         if pos[1] + i[1] >= 0:
             pygame.draw.rect(screen, color, (663 + (i[0] + pos[0]) * 20, 79 + (i[1] + pos[1]) * 20, 19, 19))
-
-figure = random.choice(figures)
-next_figure = random.choice(figures)
-type = random.randint(0, len(figure) - 1)
-next_type = random.randint(0, len(next_figure) - 1)
-pos = [9, -2]
-bricks = [0] * 20
-for i in range(20):
-    bricks[i] = [0] * 35
-color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
-next_color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
-#ставим таймер на опускание фигуры
-pygame.time.set_timer(30, 600)
-pause = False
-score = 0
-rows = 0
-tetris_fon = pygame.image.load("tetris_fon.png")
-red_pased = False
-red_button1 = pygame.image.load("red_button1.png")
-red_button2 = pygame.image.load("red_button2.png")
+    pygame.display.flip()
 
 
-while game == 1:
-    x, y = pygame.mouse.get_pos()
-    #отрисовка элементов
-    drawer()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        if event.type == 30 and not pause:
-            pos[1] += 1
-        #события на кнопки мыши
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if (((x - 651) ** 2) + ((y - 761) ** 2)) ** 0.5 <= 27:
-                red_pased = True
-                if pause:
-                    pause = False
-                else:
-                    pause = True
-        if event.type == pygame.MOUSEBUTTONUP:
-            red_pased = False
-        #все события на клавиатуру
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                exit()
-            if event.key == pygame.K_RIGHT and not pause:
-                pos[0] += 1
-                for i in figure[type]:
-                    if i[0] + pos[0] > 19:
-                        pos[0] -= 1
-                        break
-                    for j in bricks:
-                        if bricks[i[0] + pos[0]][i[1] + pos[1]] != 0:
+#сама игра
+def game(data):
+    if len(data) == 0:
+        figure = random.choice(figures)
+        next_figure = random.choice(figures)
+        type = random.randint(0, len(figure) - 1)
+        next_type = random.randint(0, len(next_figure) - 1)
+        pos = [9, -2]
+        bricks = [0] * 20
+        for i in range(20):
+            bricks[i] = [0] * 35
+        color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+        next_color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+        # ставим таймер на опускание фигуры
+        pygame.time.set_timer(30, 600)
+        score = 0
+        rows = 0
+    else:
+        score = data[0]
+        rows = data[1]
+        next_figure = data[2]
+        next_type = data[3]
+        next_color = data[4]
+        bricks = data[5]
+        figure = data[6]
+        type = data[7]
+        pos = data[8]
+        color = data[9]
+        pygame.time.set_timer(data[10][0], data[10][1])
+
+    while True:
+        x, y = pygame.mouse.get_pos()
+        # отрисовка элементов
+        drawer(score, rows, next_figure, next_type, next_color, bricks, figure, type, pos, color)
+        for event in pygame.event.get():
+            if event.type == 30:
+                pos[1] += 1
+            # события на кнопки мыши
+            # все события на клавиатуру
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return score, rows, next_figure, next_type, next_color, bricks, figure, type, pos, color, [30, 600]
+                if event.key == pygame.K_RIGHT:
+                    pos[0] += 1
+                    for i in figure[type]:
+                        if i[0] + pos[0] > 19:
                             pos[0] -= 1
                             break
+                        for j in bricks:
+                            if bricks[i[0] + pos[0]][i[1] + pos[1]] != 0:
+                                pos[0] -= 1
+                                break
 
-            #передвижение влево
-            if event.key == pygame.K_LEFT and not pause:
-                pos[0] -= 1
-                for i in figure[type]:
-                    if i[0] + pos[0] < 0:
-                        pos[0] += 1
+                # передвижение влево
+                if event.key == pygame.K_LEFT:
+                    pos[0] -= 1
+                    for i in figure[type]:
+                        if i[0] + pos[0] < 0:
+                            pos[0] += 1
+                            break
+                        if bricks[i[0] + pos[0]][i[1] + pos[1]] != 0:
+                            pos[0] += 1
+                            break
+
+                if event.key == pygame.K_DOWN:
+                    # при зажатии фигура ускоряет падение
+                    pygame.time.set_timer(30, 60)
+
+                if event.key == pygame.K_UP:
+                    # разворот фигуры
+                    type = (type + 1) % len(figure)
+                    ret = 0
+                    for i in figure[type]:
+                        if i[0] + pos[0] < 0 or i[0] + pos[0] > 19 or i[1] + pos[1] > 29 or \
+                                        bricks[i[0] + pos[0]][i[1] + pos[1]] != 0:
+                            ret = 1
+                            break
+                    if ret:
+                        type = (type - 1) % len(figure)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN:
+                    pygame.time.set_timer(30, 600)
+
+        # проверка на то, легла фигура или нет
+        for i in figure[type]:
+            if i[1] + pos[1] > 29 or bricks[i[0] + pos[0]][i[1] + pos[1]] != 0:
+                for j in figure[type]:
+                    bricks[j[0] + pos[0]][j[1] + pos[1] - 1] = color
+                score += 10
+                figure = next_figure
+                next_figure = random.choice(figures)
+                pos = [9, -2]
+                color = next_color
+                next_color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
+                type = next_type
+                next_type = random.randint(0, len(next_figure) - 1)
+                break
+
+        # проверка и удаление заполненных слоев
+        more = True
+        while more:
+            more = False
+            for i in range(34, -1, -1):
+                busy = 0
+                for j in range(0, 20):
+                    if bricks[j][i] != 0:
+                        busy += 1
+                    else:
                         break
-                    if bricks[i[0] + pos[0]][i[1] + pos[1]] != 0:
-                        pos[0] += 1
-                        break
+                if busy == 20:
+                    score += 100
+                    rows += 1
+                    more = True
+                    for i1 in range(i, 0, -1):
+                        for j1 in range(0, 20):
+                            bricks[j1][i1] = bricks[j1][i1 - 1]
 
-            if event.key == pygame.K_DOWN:
-                #при зажатии фигура ускоряет падение
-                pygame.time.set_timer(30, 60)
+#начальное окно
+while True:
+    #кнопки в началном меню
+    screen.fill((0, 0, 0))
+    x, y = pygame.mouse.get_pos()
 
-            if event.key == pygame.K_UP and not pause:
-                #разворот фигуры
-                type = (type + 1) % len(figure)
-                ret = 0
-                for i in figure[type]:
-                    if i[0] + pos[0] < 0 or i[0] + pos[0] > 19 or i[1] + pos[1] > 29 or\
-                            bricks[i[0] + pos[0]][i[1] + pos[1]] != 0:
-                        ret = 1
-                        break
-                if ret:
-                    type = (type - 1) % len(figure)
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_DOWN:
-                pygame.time.set_timer(30, 600)
+    play = 0
+    pygame.draw.rect(screen, (255, 255, 255), (750, 80, 420, 120))
+    write("PLAY", (0, 0, 0), (870, 110), 100)
+    if 750 < x < 1170 and 80 < y < 200:
+        pygame.draw.rect(screen, (180, 180, 180), (750, 80, 420, 120))
+        write("PLAY", (80, 80, 80), (870, 110), 100)
+        play = 1
 
+    save = 0
+    pygame.draw.rect(screen, (255, 255, 255), (750, 280, 420, 120))
+    write("SAVE", (0, 0, 0), (870, 310), 100)
+    if 750 < x < 1170 and 280 < y < 400:
+        pygame.draw.rect(screen, (180, 180, 180), (750, 280, 420, 120))
+        write("SAVE", (80, 80, 80), (870, 310), 100)
+        save = 1
 
-    #проверка на то, легла фигура или нет
-    for i in figure[type]:
-        if i[1] + pos[1] > 29 or bricks[i[0] + pos[0]][i[1] + pos[1]] != 0:
-            for j in figure[type]:
-                bricks[j[0] + pos[0]][j[1] + pos[1] - 1] = color
-            score += 10
-            figure = next_figure
-            next_figure = random.choice(figures)
-            pos = [9, -2]
-            color = next_color
-            next_color = (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255))
-            type = next_type
-            next_type = random.randint(0, len(next_figure) - 1)
-            break
+    download = 0
+    pygame.draw.rect(screen, (255, 255, 255), (750, 480, 420, 120))
+    write("DOWNLOAD", (0, 0, 0), (750, 510), 100)
+    if 750 < x < 1170 and 480 < y < 600:
+        pygame.draw.rect(screen, (180, 180, 180), (750, 480, 420, 120))
+        write("DOWNLOAD", (80, 80, 80), (750, 510), 100)
+        download = 1
 
-    #проверка и удаление заполненных слоев
-    more = True
-    while more:
-        more = False
-        for i in range(34, -1, -1):
-            busy = 0
-            for j in range(0, 20):
-                if bricks[j][i] != 0:
-                    busy += 1
-                else:
-                    break
-            if busy == 20:
-                score += 100
-                rows += 1
-                more = True
-                for i1 in range(i, 0, -1):
-                    for j1 in range(0, 20):
-                        bricks[j1][i1] = bricks[j1][i1 - 1]
-#############################
-    write('score:'+str(score), (255, 255, 255), (0, 0), 25)
-    for i in bricks:
-        if i[0] != 0:
-            game = 2
-    pygame.display.flip()
-for i in range(len(bricks)):
-    for m in range(len(bricks[i])):
-        if bricks[i][m] != 0:
-            pygame.draw.rect(screen, bricks[i][m], (i * 20, m * 20, 19, 19))
-write('score:' + str(score), (255, 255, 255), (None, 330))
-while game == 2:
-    pygame.display.flip()
+    ext = 0
+    pygame.draw.rect(screen, (255, 255, 255), (750, 880, 420, 120))
+    write("EXIT", (0, 0, 0), (870, 910), 100)
+    if 750 < x < 1170 and 880 < y < 1000:
+        pygame.draw.rect(screen, (180, 180, 180), (750, 880, 420, 120))
+        write("EXIT", (80, 80, 80), (870, 910), 100)
+        ext = 1
+
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                game = 0
-        if event.type == pygame.QUIT:
-            exit()
+            if ext:
+                exit()
+            if play:
+                data = game(data)
+            if save:
+                with open('data', 'w')as f:
+                    f.write(str(data))
+            if download:
+                with open('data', 'r') as f:
+                    data = eval(f.readline())
+    pygame.display.flip()
+
+
+#############################
+
+#     write('score:'+str(score), (255, 255, 255), (0, 0), 25)
+#     for i in bricks:
+#         if i[0] != 0:
+#             game = 2
+#     pygame.display.flip()
+# for i in range(len(bricks)):
+#     for m in range(len(bricks[i])):
+#         if bricks[i][m] != 0:
+#             pygame.draw.rect(screen, bricks[i][m], (i * 20, m * 20, 19, 19))
+# write('score:' + str(score), (255, 255, 255), (None, 330))
+# while game == 2:
+#     pygame.display.flip()
+#     for event in pygame.event.get():
+#         if event.type == pygame.MOUSEBUTTONDOWN:
+#             if event.button == 1:
+#                 game = 0
+#         if event.type == pygame.QUIT:
+#             exit()
